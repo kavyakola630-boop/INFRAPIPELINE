ac5fbe1c09e0fc53d5dcefb137412ecb2b5bb38d
kavyakola630-boop <kavyakola@gmail.com>
2025-11-07 15:42:31 +0530

    updated infra pipeline

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name="jenkinsnetwork"
+    Name = "jenkinsnetwork"
   }
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "ap-south-1a"
-  tags = {
-    Name="web"
-  }
 
+  tags = {
+    Name = "web"
+  }
 }
