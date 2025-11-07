1c662676153f41681f8deea858ea8a4be4f40c72
kavyakola630-boop <kavyakola@gmail.com>
2025-11-07 15:44:22 +0530

    updated infra pipeline

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.1.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -10,3 +12,4 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
